8b137aba84dfbe8ae4d92473155a5b853ccf6972
HollMax <hollmannm1@gmail.com>
2016-11-11 14:56:55 +0100

    Add files via upload

--- 05/Pr.php
+++ 05/Pr.php
@@ -1,13 +1,30 @@
 <?php
-function baseconvert($number, $basebase, $tobase=10){
+function getposition($value, $array){
+    foreach ($array as $key => $val){
+        if ($value === $val){
+            return $key;
+            }
+        }
+    }
+function anumtonum($values){
+    $alph = range("a", "z");
+    $nums = array_map("strval", range(0, 9));
+    $array = array_merge($nums, $alph);
+    foreach ($values as $key => $val){
+        $values[$key] = getposition($val, $array);
+        }
+    return $values;
+    }
+function baseconvert($number, $basebase, $tobase = 10){
     $array = str_split($number);
+    $array = anumtonum($array);
     $array = array_reverse($array);
     foreach ($array as $key => $val){
         $return += $val * ($basebase ** $key);
         }
     return $return;
-}
-$number = 1001000; 
-$base = 2;
+    }
+$number = a37334; 
+$base = 16;
 echo (baseconvert($number, $base, 10));
 ?>
